d48b6727ae445313cae0d19c431526d3890c023f
Lucas Riestra <Lucas.riestra94@gmail.com>
2023-12-22 15:22:42 +0100

    deploy38

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,24 +1,24 @@
-import fs from "fs-extra";
-import { Request, Response } from "express";
-import { uploadImage } from "../utils/cloudinary";
+import { DATA_SOURCE, mongoClient, postgresClient } from "../db/client";
 
-export const uploadPosterWithCloudinary = async (
-  req: Request,
-  res: Response
-) => {
-  try {
-    const poster = req.files?.image;
-    let posterUploaded = null;
 
-    if (poster) {
-      if ("tempFilePath" in poster) {
-        posterUploaded = await uploadImage(poster.tempFilePath);
-        await fs.unlink(poster.tempFilePath);
-      }
+export const getClient = () => {
+    if (DATA_SOURCE === 'postgres') {
+        return postgresClient
+    } else {
+        return mongoClient
     }
-    res.send({ message: "Upload Request Success", data: posterUploaded });
-  } catch (error) {
-    console.error("Error uploading poster:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+}
+
+export const convertToType = (id: string) => {
+    if (DATA_SOURCE === "postgres") {
+        return Number(id);
+    } else {
+        return id;
+    }
+}
+
+export const convertToString = (value: any): string => {
+    return String(value);
+}
+
+
