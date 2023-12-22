a1f29269cbdb80711b7ea1a8c04c3b62a2bec257
Lucas Riestra <Lucas.riestra94@gmail.com>
2023-12-22 10:44:58 +0100

    deploy10

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-//Puerta de entrada a nuestra aplicacion
-
-import app from './server'
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import app from './server';
 import config from "./config/config";
 import connect from './db/connect';
 
-const PORT = config.app.PORT
+const PORT = config.app.PORT;
 
 connect().then(() => {
     console.log("Connected to database!");
-
-    app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
 });
+
+export default (req: VercelRequest, res: VercelResponse) => {
+    return app(req, res);
+};
+
+
 
