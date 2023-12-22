f1fdc566f011289496e1f90f375ae45bc0d5758f
Lucas Riestra <Lucas.riestra94@gmail.com>
2023-12-22 10:33:52 +0100

    deploy9

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,8 @@
-import { PrismaClient as MongoClient} from "../../prisma/generated/mongodb_client";
-import { PrismaClient as PostgresClient} from "../../prisma/generated/postgresql_client";
+import { PrismaClient as MongoClient } from "../../prisma/generated/mongodb_client";
+import { PrismaClient as PostgresClient } from "../../prisma/generated/postgresql_client";
 import { DefaultArgs, PrismaClientOptions } from "@prisma/client/runtime/library";
 
-export const DATA_SOURCE = process.env.DATA_SOURCE ?? "mongo"
+export const DATA_SOURCE: string = process.env.DATA_SOURCE ?? "mongo"
 
 type ClientMongo = MongoClient<PrismaClientOptions, never, DefaultArgs>
 type ClientPostgres = PostgresClient<PrismaClientOptions, never, DefaultArgs>
@@ -10,15 +10,10 @@ type ClientPostgres = PostgresClient<PrismaClientOptions, never, DefaultArgs>
 export const mongoClient: ClientMongo = new MongoClient();
 export const postgresClient: ClientPostgres = new PostgresClient();
 
-postgresClient.$connect()
-  .then(() => console.log('Conexión con PostgreSQL exitosa'))
-  .catch((error) => console.error('Error al conectar con PostgreSQL:', error));
-
 export let prismaClient: any
 
-if(DATA_SOURCE === "postgres") {
+if (DATA_SOURCE === 'postgres') {
     prismaClient = postgresClient
 } else {
     prismaClient = mongoClient
 }
-
