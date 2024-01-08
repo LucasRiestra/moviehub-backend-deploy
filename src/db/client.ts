d70abddfb69c58a2af3ec28bd4166c7be9605a65
Lucas Riestra <Lucas.riestra94@gmail.com>
2024-01-08 09:32:26 +0100

    deploy61

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,21 @@
 import { PrismaClient } from "@prisma/client"
 
-
 const prisma = new PrismaClient()
+
+import mongoose from 'mongoose';
+
 export const DATA_SOURCE = 'mongo';
 
-export default prisma
+const connectDb = async () => {
+    try {
+      await mongoose.connect(process.env.MONGO_DB_URI);
+      console.log('MongoDB connected');
+    } catch (err) {
+      console.error('Error connecting to MongoDB', err);
+      process.exit(1);
+    }
+  };
+  
+  connectDb();
+
+export default prisma;
